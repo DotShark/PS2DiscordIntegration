2d48088fc0ca246582b939b0c1a32eb821799091
DotShark <arnaud.petit4@gmail.com>
2022-11-28 15:16:49 +0100

    Better PS2Data class tests

--- ps2_data.js
+++ ps2_data.js
@@ -194,22 +194,3 @@ class PS2Data {
 }
 
 module.exports = PS2Data;
-
-/*
-async function testPSZData() {
-	const data = new PS2Data({
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE
-	});
-
-	console.log( await data.getItemOwners(339) );
-	const items = await data.getItems();
-	items.forEach(item => console.log(`${item.id},${item.name},${item.price}`));
-	console.log( await data.getItemInfos(213) );
-	console.log( await data.getPlayerWallet("STEAM_0:1:73530340") );
-}
-
-testPSZData();
-*/
